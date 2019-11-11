7a6302d9b4ba65e4f9685490717ff4a6eaed5e04
Alex Purkhalo <alexpurhalo@gmail.com>
2019-11-11 18:08:05 +0200

    React DevTools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux'
+import { compose, applMiddleware, createStore, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export default (state, reducers) => createStore(combineReducers(reducers), state)
+export default (state, reducers) => createStore(combineReducers(reducers), state, composeWithDevTools())
 // TODO: add the store specs
